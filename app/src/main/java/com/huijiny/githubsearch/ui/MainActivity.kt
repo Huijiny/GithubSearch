57beb8f6996f525ffc03a331c923f7c526616400
Huijiny <jiin20803@gmail.com>
2021-10-26 20:04:49 +0900

    [#4] 검색창 및 리사이클러뷰 아이템 xml 구현

--- app/src/main/java/com/huijiny/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/com/huijiny/githubsearch/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.huijiny.githubsearch
+package com.huijiny.githubsearch.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.huijiny.githubsearch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
